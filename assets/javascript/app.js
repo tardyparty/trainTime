f7ae44171f767ab37e29f86746420739500a588a
tardyparty <tardyparty@users.noreply.github.com>
2019-09-10 16:51:25 -0500

    html skel and firebase config

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCSGDET0_BTeAodgwRGWqwrVt3h9tMDsag",
+    authDomain: "traintime-cd28a.firebaseapp.com",
+    databaseURL: "https://traintime-cd28a.firebaseio.com",
+    projectId: "traintime-cd28a",
+    storageBucket: "",
+    messagingSenderId: "1052205768593",
+    appId: "1:1052205768593:web:ad9dfdc8d843a600118430"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
