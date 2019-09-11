99384d27f78d521587e28ffe495fefba767a7d47
tardyparty <tardyparty@users.noreply.github.com>
2019-09-10 19:23:09 -0500

    adds new trai to database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,48 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 
+// variables
+const data = firebase.database();
+
+var name = '';
+var dest = '';
+var time = 0;
+var freq = 0;
+
+
+// add train func
+$(document).on("click", "#add-train", function(event){
+    event.preventDefault();
+
+    name = $("#name-input").val().trim();
+    dest = $("#destination-input").val().trim();
+    time = $("#starttime-input").val().trim();
+    freq = $("#frequency-input").val().trim();
+
+    // send to firebase
+    data.ref().push({
+        name: name,
+        destination: dest,
+        time: time,
+        freguency: freq,
+    });
+
+    $("#name-input").val('');
+    $("#destination-input").val('');
+    $("#starttime-input").val('');
+    $("#frequency-input").val('');
+
+});
+
+data.ref().on("child_added", function(snapshot){
+
+     console.log(snapshot.val().name);
+     console.log(snapshot.val().destination);
+     console.log(snapshot.val().time);
+     console.log(snapshot.val().freq);
+
+    $("#traintable").append(`
+        
+    `);
+
+});
